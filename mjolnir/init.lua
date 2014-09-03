dccd32dfb59220b67f7d649ce246aaef3c8ded2d
Alexander Korobov <akorobov@gmail.com>
2014-09-03 13:33:15 -0700

    mjolnir + 2-dim grid module

---
+++ mjolnir/init.lua
@@ -0,0 +1,45 @@
+local application = require "mjolnir.application"
+local window = require "mjolnir.window"
+local hotkey = require "mjolnir.hotkey"
+local fnutils = require "mjolnir.fnutils"
+local alert = require "mjolnir.alert"
+-- local grid = require "mjolnir.sd.grid"
+
+local grid = require "grid"
+
+local mash = {"cmd", "alt", "ctrl"}
+local mashshift = {"cmd", "alt", "shift"}
+
+-- requires: grid, fnutils, alert
+
+local function opendictionary()
+  application.launchorfocus("Dictionary")
+end
+
+hotkey.bind(mash, 'D', opendictionary)
+
+hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
+hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
+
+hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+
+hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
+
+hotkey.bind(mash, 'M', grid.maximize_window)
+
+hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
+
+hotkey.bind(mash, 'J', grid.pushwindow_down)
+hotkey.bind(mash, 'K', grid.pushwindow_up)
+hotkey.bind(mash, 'H', grid.pushwindow_left)
+hotkey.bind(mash, 'L', grid.pushwindow_right)
+
+hotkey.bind(mash, 'U', grid.resizewindow_taller)
+hotkey.bind(mash, 'Y', grid.resizewindow_shorter)
+hotkey.bind(mash, 'O', grid.resizewindow_wider)
+hotkey.bind(mash, 'I', grid.resizewindow_thinner)
