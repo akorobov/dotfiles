d04bb29da24a5c39a46d18e397a5a408aecc8fc0
Alexander Korobov <akorobov@gmail.com>
2015-01-15 13:07:40 -0800

    fix extention method names

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,7 +2,7 @@ local grid = hs.grid
 local window = hs.window
 local hotkey = hs.hotkey
 
-window.animation_duration = 0
+window.animationDuration = 0
 grid.GRIDHEIGHT = 8
 grid.GRIDWIDTH = 16
 grid.MARGINX = 0
@@ -17,30 +17,29 @@ local mashshift = {"cmd", "alt", "shift"}
 --   application.launchorfocus("Dictionary")
 -- end
 
-hotkey.bind(mash, 'D', opendictionary)
 
-hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
-hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
+hotkey.bind(mash, ';', function() grid.snap(window.focusedWindow()) end)
+hotkey.bind(mash, "'", function() hs.fnutils.map(window.visibleWindows(), grid.snap) end)
 
-hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
-hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+hotkey.bind(mash, '=', function() grid.adjustWidth(1) end)
+hotkey.bind(mash, '-', function() grid.adjustWidth(-1) end)
 
-hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
-hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
-hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
-hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
+hotkey.bind(mashshift, 'H', function() window.focusedWindow():focusWindowWest() end)
+hotkey.bind(mashshift, 'L', function() window.focusedWindow():focusWindowEast() end)
+hotkey.bind(mashshift, 'K', function() window.focusedWindow():focusWindowNorth() end)
+hotkey.bind(mashshift, 'J', function() window.focusedWindow():focusWindowSouth() end)
 
-hotkey.bind(mash, 'M', grid.maximize_window)
+hotkey.bind(mash, 'M', grid.maximizeWindow)
 
-hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
-hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
+hotkey.bind(mash, 'N', grid.pushWindowNextScreen)
+hotkey.bind(mash, 'P', grid.pushindowPrevScreen)
 
-hotkey.bind(mash, 'J', grid.pushwindow_down)
-hotkey.bind(mash, 'K', grid.pushwindow_up)
-hotkey.bind(mash, 'H', grid.pushwindow_left)
-hotkey.bind(mash, 'L', grid.pushwindow_right)
+hotkey.bind(mash, 'J', grid.pushWindowDown)
+hotkey.bind(mash, 'K', grid.pushWindowUp)
+hotkey.bind(mash, 'H', grid.pushWindowLeft)
+hotkey.bind(mash, 'L', grid.pushWindowRight)
 
-hotkey.bind(mash, 'U', grid.resizewindow_taller)
-hotkey.bind(mash, 'Y', grid.resizewindow_shorter)
-hotkey.bind(mash, 'O', grid.resizewindow_wider)
-hotkey.bind(mash, 'I', grid.resizewindow_thinner)
+hotkey.bind(mash, 'U', grid.resizeWindowTaller)
+hotkey.bind(mash, 'Y', grid.resizeWindowShorter)
+hotkey.bind(mash, 'O', grid.resizeWindowWider)
+hotkey.bind(mash, 'I', grid.resizeWindowThinner)
